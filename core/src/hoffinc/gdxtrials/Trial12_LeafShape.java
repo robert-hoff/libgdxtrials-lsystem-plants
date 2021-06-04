0af0a9c3819bd927cfa56e6676decdcfc39cf7c2
Hoff Inc <hoff.rj@gmail.com>
2021-06-04 12:03:05 +0100

    tidy

--- core/src/hoffinc/gdxtrials/Trial12_LeafShape.java
+++ core/src/hoffinc/gdxtrials/Trial12_LeafShape.java
@@ -1,9 +1,7 @@
 package hoffinc.gdxtrials;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Stack;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -13,20 +11,14 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.CylinderShapeBuilder;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
@@ -205,15 +197,9 @@ public class Trial12_LeafShape extends ApplicationAdapter {
     Matrix4 transform = new Matrix4();
     Stack<Matrix4> stack = new Stack<>();
 
-
     public TurtleDrawer(Model model, float path_len) {
       turtlePath = model;
       this.PATH_LEN = path_len;
-    }
-    public TurtleDrawer() {
-      turtlePath = TurtlePathModel.buildTurtlePath();
-      this.PATH_LEN = 0.1f;
-
     }
 
     public void push() {
@@ -223,7 +209,6 @@ public class Trial12_LeafShape extends ApplicationAdapter {
     public void pop() {
       transform = stack.pop();
     }
-
 
     // (doesn't draw anything)
     public void walk() {
@@ -282,15 +267,9 @@ public class Trial12_LeafShape extends ApplicationAdapter {
 
 
 
-
-
-
     public Array<ModelInstance> getPaths() {
       return paths;
     }
-
-
-
 
     public static void showVector3(Vector3 vec) {
       System.err.printf("%9.4f %9.4f %9.4f \n", vec.x, vec.y, vec.z);
@@ -303,55 +282,6 @@ public class Trial12_LeafShape extends ApplicationAdapter {
   }
 
 
-
-
-
-  // Path adjusted slightly to fit this example
-  // a cylinder, pointing upwards (z-axis)
-  private static class TurtlePathModel {
-    private static final float CYL_DIAM = 0.01f;
-    private static final float CYL_LENGTH = 0.1f;
-    private static final int MESH_RES = 5;
-    public static Model buildTurtlePath() {
-      ModelBuilder modelBuilder = new ModelBuilder();
-      modelBuilder.begin();
-      MeshPartBuilder turtlePathBuilder = modelBuilder.part("turtle_path", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal,getTurtleMaterial());
-      turtlePathBuilder.setVertexTransform(new Matrix4().translate(0,0,CYL_LENGTH/2).rotate(1,0,0,90));
-      CylinderShapeBuilder.build(turtlePathBuilder, CYL_DIAM, CYL_LENGTH, CYL_DIAM, MESH_RES);
-      return modelBuilder.end();
-    }
-    private static Material getTurtleMaterial() {
-      int color = 0x009933;  // green
-      int color_rgba8888 = (color << 8) + 0xff;
-      Material turtleMat = new Material(ColorAttribute.createDiffuse(new Color(color_rgba8888)));
-      return turtleMat;
-    }
-  }
-
-
-
-
-  static List<Character> lSystemProduction(int n, String s, Map<Character,String> p) {
-    List<Character> symbols = new ArrayList<>();
-    for(char c : s.toCharArray()){
-      symbols.add(c);
-    }
-    for (int i = 1; i <= n; i++) {
-      List<Character> nextSymbols = new ArrayList<>();
-      for(char c : symbols){
-        String p_rule = p.get(c);
-        if (p_rule != null) {
-          for(char c_prod : p_rule.toCharArray()){
-            nextSymbols.add(c_prod);
-          }
-        } else {
-          nextSymbols.add(c);
-        }
-      }
-      symbols = nextSymbols;
-    }
-    return symbols;
-  }
 
 
   static List<Character> lSystemConvertString(String s) {
